afdbd88be1b864643e5d36a999f01dea7cdb0884
Kayla Rosario <rosariok314@gmail.com>
2022-06-29 20:20:57 -0400

    functional search btn, history commands

--- assets/script/index.js
+++ assets/script/index.js
@@ -38,7 +38,8 @@ const day2Humid = document.getElementById("#day2Humid");
 const day3Humid = document.getElementById("#day3Humid");
 const day4Humid = document.getElementById("#day4Humid");
 const day5Humid = document.getElementById("#day5Humid");
-// function
+
+// get location function
 function getLocation(cityName) {
     const urlKey = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myKeyAPI;
 
@@ -51,9 +52,66 @@ function getLocation(cityName) {
             fetch(coorUrl).then(function(response) {
                 response.json().then(function(data) {
                     console.log(data);
-                    const weatherIcon = $("<img src='http://openweathermap.org/img/wn/10d@2x.png')
-                })
-            })
-        })
-    })
+                    const weatherIcon = $("<img src='http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png'>"
+                    );
+
+                    locationName.textContent(cityName + " (" + moment().format("l") + ")");
+                    locationName.append(weatherIcon);
+                    temperature.text("Temp: " + data.current.temperature + "°F");
+                    wind.text("Wind Speeds: " + data.current.windspeed + " MPH");
+                    humid.text("Humidity: " + data.current.humidity + " %");
+                    uv.text("UV Index: " + data.current.uv);
+
+                    for (let i = 1; i < 6; i++) {
+                        window["day" + i + "Date"].text(
+                            moment.unix(data.daily[i].dt).format("l")
+                        );
+                        window["day" + i + "Icon"]
+                        .empty()
+                        .append(
+                            $("<img src='http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png'>"
+                            )
+                            );
+                        window["plus" + i + "Temp"].text(
+                            "Temp: " + data.daily[i].temperature + " °F"
+                            );
+                        window["plus" + i + "Wind"].text(
+                            "Wind: " + data.daily[i].windspeed + " MPH"
+                            );
+                        window["plus" + i + "Humid"].text(
+                                "Humidity: " + data.daily[i].humidity + " %"
+                        );
+                    }
+                });
+            });
+        });
+    });
 }
+
+// submit button
+function submitSearch(cityName) {
+    const newBtn = $("<button></button>");
+    newBtn.text(cityName);
+    newBtn.attr("type", "button");
+    newBtn.addClass("btn");
+    newBtn.addClass("btn-secondary");
+    searchHistory.append(newBtn);
+}
+
+// saving searched city info
+function clickSearch() {
+    const citiesInput = cityInput.val();
+    submitSearch(citiesInput);
+    getLocation(citiesInput);
+}
+
+// search history cmds
+function historyBtn(event) {
+    event.stopPropagation();
+    const cityHistory = event.target.innerText;
+    getLocation(cityHistory);
+}
+
+// back
+searchBtn.on("click", clickSearch);
+searchHistory.on("click", historyBtn);
